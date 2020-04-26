560168ba02109819d34e34dfe93b38a44690934d
Sri <srijayakumar99@gmail.com>
2020-04-25 20:59:23 -0400

    make filter buttons radio buttons

--- frontend/src/app/filter/filter.component.ts
+++ frontend/src/app/filter/filter.component.ts
@@ -1,32 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
-
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit, OnChanges {
+  @Input() source: any
+  @Input() clickedSource: any
+  @Output() clicked: EventEmitter<any> = new EventEmitter(); 
+  constructor() { }
 
-  constructor(public breakpointObserver: BreakpointObserver) { }
-  sources = ["Reddit", "Google"]
-  column = "col-3"
-  clicked = false
+  didClick = false
   ngOnInit(): void {
-    this.breakpointObserver
-      .observe(['(max-width: 1000px)'])
-      .subscribe((state: BreakpointState) => {
-        if(state.matches) {
-          this.column = "col-6"
-        } else {
-          this.column = "col-3"
-        }
-      })
+    console.log(
+      this.source + this.clickedSource
+    )
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    if(changes.clickedSource.currentValue == this.source){
+      this.didClick = true
+    } else {
+      this.didClick = false
+    }
   }
 
   onClick() {
-    this.clicked = !this.clicked
+    if(this.didClick){
+      this.clicked.emit(null)
+    } else {
+      this.clicked.emit(this.source)
+    }
   }
 
 }
--- frontend/src/app/radiobutton/radiobutton.component.ts
+++ frontend/src/app/radiobutton/radiobutton.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-radiobutton',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./radiobutton.component.css']
 })
 export class RadiobuttonComponent implements OnInit {
-
-  constructor() { }
+  sources = ["Reddit", "Google"]
+  column = "col-3"
+  clickedSource: any
+  constructor(public breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.breakpointObserver
+    .observe(['(max-width: 1000px)'])
+    .subscribe((state: BreakpointState) => {
+      if(state.matches) {
+        this.column = "col-6"
+      } else {
+        this.column = "col-3"
+      }
+    })
+  }
+
+  onClicked(value) {
+    this.clickedSource = value
   }
 
 }
